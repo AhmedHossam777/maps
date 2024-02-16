8ca01466b39def8e24ded9fec8ef0952ebd0fcc6
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-02-16 22:59:53 +0200

    finish company class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
-console.log(user);
+console.log('user: ', user);
+
+const company = new Company();
+console.log('company: ', company);
