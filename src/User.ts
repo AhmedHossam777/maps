cf0ec7ef976ed370cac8338c62753cb8b81516e9
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-02-16 20:23:45 +0200

    complete initial implementaion of User class

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,21 @@
-class User{}
+// we will user faker package to generate name, lat and lng
+import { faker } from '@faker-js/faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
+
+const user = new User();
+console.log(user.location);
